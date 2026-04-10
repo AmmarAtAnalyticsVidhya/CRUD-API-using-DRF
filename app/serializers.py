cd7c8f2111aea405a3ad549463de307ea2cfb4d1
AmmarAtAnalyticsVidhya <ammar.adil@analyticsvidhya.com>
2026-04-10 16:45:43 +0530

    added date validations for not to be in future

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,13 @@
 from .models import Book
 from rest_framework import serializers
+from django.utils import timezone
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def validate_published_date(self, value):
+        if value > timezone.now().date():
+            raise serializers.ValidationError("Published date cannot be in the future.")
+        return value
