b55eb6b5c0fc63b108a6a8cba32a09b8ad931466
AmmarAtAnalyticsVidhya <ammar.adil@analyticsvidhya.com>
2026-04-14 12:40:53 +0530

    added the basic auth globally

--- app/views.py
+++ app/views.py
@@ -9,5 +9,5 @@ from rest_framework.permissions import IsAuthenticated
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+ #   authentication_classes = [BasicAuthentication]
+#  permission_classes = [IsAuthenticated]
