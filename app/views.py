32d7db1eac190e00abf82fcc277d680dedd9c917
AmmarAtAnalyticsVidhya <ammar.adil@analyticsvidhya.com>
2026-04-14 12:28:50 +0530

    created admin superuser and added basic authentication

--- app/views.py
+++ app/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
