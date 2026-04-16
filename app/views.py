8e5941eaa46829ff975a5c44bed814bdcce8d6a9
AmmarAtAnalyticsVidhya <ammar.adil@analyticsvidhya.com>
2026-04-16 16:02:13 +0530

    learnt more permission classes

--- app/views.py
+++ app/views.py
@@ -3,13 +3,16 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     authentication_classes = [SessionAuthentication]
-    permission_classes = [IsAdminUser]
+# permission_classes = [IsAdminUser] # Uncomment this line to restrict access to admin users only
 # permission_classes = [IsAuthenticated]  # Uncomment this line to require authentication for all users
 # permission_classes = [AllowAny]  # Uncomment this line to allow unrestricted access to all users
+# permission_classes = [IsAuthenticatedOrReadOnly]  # Uncomment this line to allow read-only access for unauthenticated users and full access for authenticated users
+# permission_classes = [DjangoModelPermissions]  # Uncomment this line to use Django's built-in model permissions for fine-grained access control
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]  # Uncomment this line to use Django's built-in model permissions for fine-grained access control
